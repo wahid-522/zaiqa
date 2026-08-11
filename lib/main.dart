6cf05cc0f7a1434ac581f873e16b354e50d53a92
wahid72701@gmail.com <wahid72701@gmail.com>
2026-08-11 16:22:47 +0500

    Add firebase_core, firebase_auth, and cloud_firestore packages and initialize Firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'core/routing/app_router.dart';
 import 'core/theme/app_theme.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: ZaiqaApp(),
